d2bdd374390ff0332f8b53d660960ef4f1000fca
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-03-31 10:14:13 -0300

    56rh

--- Exercicio7.js
+++ Exercicio7.js
@@ -6,5 +6,5 @@ sabor = [["chocolate", 1.50],["Morango", 2.50],["Creme", 2.50],["Manga", 3.20],[
 
 
     escolha = parseInt(prompt("Escolha o sabor, 1 para chocolate, 2 Morango, 3 Creme, 4 Manga, 5 Malancia, 6 Vanila, 7 Céu azul, 8 Brownie, 9 Hawaiano"));
-    alert("seu picolé vai sair por R$" + sabor[[escolha - 1][1]]);
+    alert("seu picolé vai sair por R$" + [saborescolha - 1[1]]);
 }
